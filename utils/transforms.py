0ac9f8171530fa1f0ef3eb7725fc4fc17fb69372
zhunzhong07 <zhun.zhong@foxmail.com>
2017-09-25 10:51:29 +0800

    new file:   utils/__init__.py 	new file:   utils/eval.py 	new file:   utils/logger.py 	new file:   utils/misc.py 	new file:   utils/transforms.py 	new file:   utils/visualize.py

---
+++ utils/transforms.py
@@ -0,0 +1,47 @@
+from __future__ import absolute_import
+
+from torchvision.transforms import *
+
+import numpy as np
+import torch
+
+class RandomErasing(object):
+    def __init__(self, EPSILON = 0.5, sl = 0.02, sh = 0.4, r1 = 0.3, mean=[0.4914, 0.4822, 0.4465]):
+        self.EPSILON = EPSILON
+        self.mean = mean
+        self.sl = sl
+        self.sh = sh
+        self.r1 = r1
+       
+    def __call__(self, img):
+
+        if random.uniform(0, 1) > self.EPSILON:
+            return img
+
+        for attempt in range(100):
+            area = img.size()[1] * img.size()[2]
+       
+            target_area = random.uniform(self.sl, self.sh) * area
+            aspect_ratio = random.uniform(self.r1, 1/self.r1)
+
+            h = int(round(math.sqrt(target_area * aspect_ratio)))
+            w = int(round(math.sqrt(target_area / aspect_ratio)))
+
+            if w <= img.size()[2] and h <= img.size()[1]:
+                x1 = random.randint(0, img.size()[1] - h)
+                y1 = random.randint(0, img.size()[2] - w)
+                if img.size()[0] == 3:
+                    #img[0, x1:x1+h, y1:y1+w] = random.uniform(0, 1)
+                    #img[1, x1:x1+h, y1:y1+w] = random.uniform(0, 1)
+                    #img[2, x1:x1+h, y1:y1+w] = random.uniform(0, 1)
+                    img[0, x1:x1+h, y1:y1+w] = self.mean[0]
+                    img[1, x1:x1+h, y1:y1+w] = self.mean[1]
+                    img[2, x1:x1+h, y1:y1+w] = self.mean[2]
+                    #img[:, x1:x1+h, y1:y1+w] = torch.from_numpy(np.random.rand(3, h, w))
+                else:
+                    img[0, x1:x1+h, y1:y1+w] = self.mean[1]
+                    # img[0, x1:x1+h, y1:y1+w] = torch.from_numpy(np.random.rand(1, h, w))
+                return img
+
+        return img
+
