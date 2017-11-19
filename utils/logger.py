4cf2d0e0e82f22da6d2ad800f531ca9423c655f7
Zhun Zhong <zhun.zhong@foxmail.com>
2017-11-19 23:04:47 +1100

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,6 +1,8 @@
 # A simple torch style logger
 # (C) Wei YANG 2017
 from __future__ import absolute_import
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import os
 import sys
