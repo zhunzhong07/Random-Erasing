96dfe1db1392ba21c96a048a6a76a3f4eb877fa1
Tran Dai Phu <113273365+PhuTd03@users.noreply.github.com>
2023-06-11 18:23:43 +0700

    Update transforms.py
    fix something to fix with the fashion-mnist code

--- transforms.py
+++ transforms.py
@@ -19,7 +19,7 @@ class RandomErasing(object):
     mean: erasing value
     -------------------------------------------------------------------------------------
     '''
-    def __init__(self, probability = 0.5, sl = 0.02, sh = 0.4, r1 = 0.3, mean=[0.4914, 0.4822, 0.4465]):
+    def __init__(self, probability = 0.5, sl = 0.02, sh = 0.4, r1 = 0.3, mean=[0.4914]):
         self.probability = probability
         self.mean = mean
         self.sl = sl
@@ -30,26 +30,23 @@ class RandomErasing(object):
 
         if random.uniform(0, 1) > self.probability:
             return img
-
-        for attempt in range(100):
-            area = img.size()[1] * img.size()[2]
+        
+        img = img.copy()
+        h, w = img.shape
+        
+        area = h * w
        
-            target_area = random.uniform(self.sl, self.sh) * area
-            aspect_ratio = random.uniform(self.r1, 1/self.r1)
+        target_area = random.uniform(self.sl, self.sh) * area
+        aspect_ratio = random.uniform(self.r1, 1/self.r1)
 
-            h = int(round(math.sqrt(target_area * aspect_ratio)))
-            w = int(round(math.sqrt(target_area / aspect_ratio)))
+        erase_h = int(round(math.sqrt(target_area * aspect_ratio)))
+        erase_w = int(round(math.sqrt(target_area / aspect_ratio)))
 
-            if w < img.size()[2] and h < img.size()[1]:
-                x1 = random.randint(0, img.size()[1] - h)
-                y1 = random.randint(0, img.size()[2] - w)
-                if img.size()[0] == 3:
-                    img[0, x1:x1+h, y1:y1+w] = self.mean[0]
-                    img[1, x1:x1+h, y1:y1+w] = self.mean[1]
-                    img[2, x1:x1+h, y1:y1+w] = self.mean[2]
-                else:
-                    img[0, x1:x1+h, y1:y1+w] = self.mean[0]
-                return img
+        if erase_h < h and erase_w < w:
+            x1 = random.randint(0, img.size()[1] - h)
+            y1 = random.randint(0, img.size()[2] - w)
+            
+            img[x1 : x1 + erase_h, y1 : y1 + erase_w] = self.mean[0]
 
         return img
 
