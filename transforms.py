03a3222a002e21ef65c85550672bc89c2853c7b2
Tran Dai Phu <113273365+PhuTd03@users.noreply.github.com>
2023-06-11 18:37:14 +0700

    Update transforms.py
    fix something to fix with the fashion-mnist code

--- transforms.py
+++ transforms.py
@@ -9,47 +9,31 @@ import numpy as np
 import torch
 
 class RandomErasing(object):
-    '''
-    Class that performs Random Erasing in Random Erasing Data Augmentation by Zhong et al. 
-    -------------------------------------------------------------------------------------
-    probability: The probability that the operation will be performed.
-    sl: min erasing area
-    sh: max erasing area
-    r1: min aspect ratio
-    mean: erasing value
-    -------------------------------------------------------------------------------------
-    '''
-    def __init__(self, probability = 0.5, sl = 0.02, sh = 0.4, r1 = 0.3, mean=[0.4914, 0.4822, 0.4465]):
+    def __init__(self, probability=0.5, sl=0.02, sh=0.4, r1=0.3, mean=[0.4914]):
         self.probability = probability
         self.mean = mean
         self.sl = sl
         self.sh = sh
         self.r1 = r1
-       
-    def __call__(self, img):
 
+    def __call__(self, img):
         if random.uniform(0, 1) > self.probability:
             return img
 
-        for attempt in range(100):
-            area = img.size()[1] * img.size()[2]
-       
-            target_area = random.uniform(self.sl, self.sh) * area
-            aspect_ratio = random.uniform(self.r1, 1/self.r1)
+        img = img.copy()
+        h, w = img.shape
 
-            h = int(round(math.sqrt(target_area * aspect_ratio)))
-            w = int(round(math.sqrt(target_area / aspect_ratio)))
+        area = h * w
+        target_area = random.uniform(self.sl, self.sh) * area
+        aspect_ratio = random.uniform(self.r1, 1 / self.r1)
 
-            if w < img.size()[2] and h < img.size()[1]:
-                x1 = random.randint(0, img.size()[1] - h)
-                y1 = random.randint(0, img.size()[2] - w)
-                if img.size()[0] == 3:
-                    img[0, x1:x1+h, y1:y1+w] = self.mean[0]
-                    img[1, x1:x1+h, y1:y1+w] = self.mean[1]
-                    img[2, x1:x1+h, y1:y1+w] = self.mean[2]
-                else:
-                    img[0, x1:x1+h, y1:y1+w] = self.mean[0]
-                return img
+        erase_h = int(round(math.sqrt(target_area * aspect_ratio)))
+        erase_w = int(round(math.sqrt(target_area / aspect_ratio)))
+
+        if erase_h < h and erase_w < w:
+            x1 = random.randint(0, h - erase_h)
+            y1 = random.randint(0, w - erase_w)
+
+            img[x1:x1 + erase_h, y1:y1 + erase_w] = self.mean[0]
 
         return img
-
